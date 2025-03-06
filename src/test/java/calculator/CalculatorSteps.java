946a41a438ccd9c578f2ccf23c729983d0cf2bb1
Clément Allard <191024@umons.ac.be>
2025-03-06 13:55:32 +0100

    Fixed CalculatorSteps

--- src/test/java/calculator/CalculatorSteps.java
+++ src/test/java/calculator/CalculatorSteps.java
@@ -106,40 +106,8 @@ public class CalculatorSteps {
 		}
 	}
 
-	@Given("the operation is {string}")
-	public void the_operation_is(String string) {
-		int value1 = 8;
-		int value2 = 6;
-		params = new ArrayList<>();
-		params.add(new MyNumber(value1));
-		params.add(new MyNumber(value2));
-		try {
-			//construct another type of operation depending on the input value
-			//of the parameterised test
-			switch (string) {
-				case "+"	->	op = new Plus(params);
-				case "-"	->	op = new Minus(params);
-				case "*"	->	op = new Times(params);
-				case "/"	->	op = new Divides(params);
-				default		->	fail();
-			}
-		} catch (IllegalConstruction e) {
-			fail();
-		}
-		testNotations(string, value1, value2, op);
-	}
-
-	/* This is an auxiliary method to avoid code duplication.
-	 */
-	void testNotation(String s,Operation o,Notation n) {
-		assertEquals(s, o.toString(n));
-		o.notation = n;
-		assertEquals(s, o.toString());
-	}
-
-	/* This is an auxiliary method to avoid code duplication.
-	 */
-	void testNotations(String symbol,int value1,int value2,Operation op) {
+	@Then("the operation {string} and number {int} and {int} are correct")
+	public void testNotations(String symbol, int value1, int value2){
 		//prefix notation:
 		testNotation(symbol +" (" + value1 + ", " + value2 + ")", op, Notation.PREFIX);
 		//infix notation:
@@ -148,6 +116,12 @@ public class CalculatorSteps {
 		testNotation("(" + value1 + ", " + value2 + ") " + symbol, op, Notation.POSTFIX);
 	}
 
+	/* This is an auxilary method to avoid code duplication. */
+	private void testNotation(String s,Operation o,Notation n) {
+		assertEquals(s, o.toString(n));
+		op.notation = n;
+		assertEquals(s, o.toString());
+	}
 
 	@Then("the operation evaluates to {int}")
 	public void thenTheOperationEvaluatesTo(int val) {
