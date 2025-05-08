04f450ebd00418fdbaafdf2c364bc2e80baa1084
amanzio <200291@umons.ac.be>
2025-05-08 20:09:12 +0200

    Fix: Comment empty functions bodies.

--- src/main/java/helper/antlr4/ExpressionBaseListener.java
+++ src/main/java/helper/antlr4/ExpressionBaseListener.java
@@ -17,336 +17,464 @@ public class ExpressionBaseListener implements ExpressionListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterExpr(ExpressionParser.ExprContext ctx) { }
+	@Override public void enterExpr(ExpressionParser.ExprContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitExpr(ExpressionParser.ExprContext ctx) { }
+	@Override public void exitExpr(ExpressionParser.ExprContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterPrefixExpression(ExpressionParser.PrefixExpressionContext ctx) { }
+	@Override public void enterPrefixExpression(ExpressionParser.PrefixExpressionContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitPrefixExpression(ExpressionParser.PrefixExpressionContext ctx) { }
+	@Override public void exitPrefixExpression(ExpressionParser.PrefixExpressionContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterPrefixNumber(ExpressionParser.PrefixNumberContext ctx) { }
+	@Override public void enterPrefixNumber(ExpressionParser.PrefixNumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitPrefixNumber(ExpressionParser.PrefixNumberContext ctx) { }
+	@Override public void exitPrefixNumber(ExpressionParser.PrefixNumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterPostfixExpression(ExpressionParser.PostfixExpressionContext ctx) { }
+	@Override public void enterPostfixExpression(ExpressionParser.PostfixExpressionContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitPostfixExpression(ExpressionParser.PostfixExpressionContext ctx) { }
+	@Override public void exitPostfixExpression(ExpressionParser.PostfixExpressionContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterPostfixNumber(ExpressionParser.PostfixNumberContext ctx) { }
+	@Override public void enterPostfixNumber(ExpressionParser.PostfixNumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitPostfixNumber(ExpressionParser.PostfixNumberContext ctx) { }
+	@Override public void exitPostfixNumber(ExpressionParser.PostfixNumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterSingleTerm(ExpressionParser.SingleTermContext ctx) { }
+	@Override public void enterSingleTerm(ExpressionParser.SingleTermContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitSingleTerm(ExpressionParser.SingleTermContext ctx) { }
+	@Override public void exitSingleTerm(ExpressionParser.SingleTermContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterInfixExpressionAddSub(ExpressionParser.InfixExpressionAddSubContext ctx) { }
+	@Override public void enterInfixExpressionAddSub(ExpressionParser.InfixExpressionAddSubContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitInfixExpressionAddSub(ExpressionParser.InfixExpressionAddSubContext ctx) { }
+	@Override public void exitInfixExpressionAddSub(ExpressionParser.InfixExpressionAddSubContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterInfixExpressionImplicitMul(ExpressionParser.InfixExpressionImplicitMulContext ctx) { }
+	@Override public void enterInfixExpressionImplicitMul(ExpressionParser.InfixExpressionImplicitMulContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitInfixExpressionImplicitMul(ExpressionParser.InfixExpressionImplicitMulContext ctx) { }
+	@Override public void exitInfixExpressionImplicitMul(ExpressionParser.InfixExpressionImplicitMulContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterInfixExpressionMulDiv(ExpressionParser.InfixExpressionMulDivContext ctx) { }
+	@Override public void enterInfixExpressionMulDiv(ExpressionParser.InfixExpressionMulDivContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitInfixExpressionMulDiv(ExpressionParser.InfixExpressionMulDivContext ctx) { }
+	@Override public void exitInfixExpressionMulDiv(ExpressionParser.InfixExpressionMulDivContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterSingleFactor(ExpressionParser.SingleFactorContext ctx) { }
+	@Override public void enterSingleFactor(ExpressionParser.SingleFactorContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitSingleFactor(ExpressionParser.SingleFactorContext ctx) { }
+	@Override public void exitSingleFactor(ExpressionParser.SingleFactorContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterInfixExpressionWithParenthesis(ExpressionParser.InfixExpressionWithParenthesisContext ctx) { }
+	@Override public void enterInfixExpressionWithParenthesis(ExpressionParser.InfixExpressionWithParenthesisContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitInfixExpressionWithParenthesis(ExpressionParser.InfixExpressionWithParenthesisContext ctx) { }
+	@Override public void exitInfixExpressionWithParenthesis(ExpressionParser.InfixExpressionWithParenthesisContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterInfixExpressionNegate(ExpressionParser.InfixExpressionNegateContext ctx) { }
+	@Override public void enterInfixExpressionNegate(ExpressionParser.InfixExpressionNegateContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitInfixExpressionNegate(ExpressionParser.InfixExpressionNegateContext ctx) { }
+	@Override public void exitInfixExpressionNegate(ExpressionParser.InfixExpressionNegateContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterFactorNumber(ExpressionParser.FactorNumberContext ctx) { }
+	@Override public void enterFactorNumber(ExpressionParser.FactorNumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitFactorNumber(ExpressionParser.FactorNumberContext ctx) { }
+	@Override public void exitFactorNumber(ExpressionParser.FactorNumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterUnaryFunction(ExpressionParser.UnaryFunctionContext ctx) { }
+	@Override public void enterUnaryFunction(ExpressionParser.UnaryFunctionContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitUnaryFunction(ExpressionParser.UnaryFunctionContext ctx) { }
+	@Override public void exitUnaryFunction(ExpressionParser.UnaryFunctionContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterComplexAtom(ExpressionParser.ComplexAtomContext ctx) { }
+	@Override public void enterComplexAtom(ExpressionParser.ComplexAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitComplexAtom(ExpressionParser.ComplexAtomContext ctx) { }
+	@Override public void exitComplexAtom(ExpressionParser.ComplexAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterNotComplex(ExpressionParser.NotComplexContext ctx) { }
+	@Override public void enterNotComplex(ExpressionParser.NotComplexContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitNotComplex(ExpressionParser.NotComplexContext ctx) { }
+	@Override public void exitNotComplex(ExpressionParser.NotComplexContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterScientificAtom(ExpressionParser.ScientificAtomContext ctx) { }
+	@Override public void enterENumber(ExpressionParser.ENumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitScientificAtom(ExpressionParser.ScientificAtomContext ctx) { }
+	@Override public void exitENumber(ExpressionParser.ENumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterNotScientific(ExpressionParser.NotScientificContext ctx) { }
+	@Override public void enterScientificAtom(ExpressionParser.ScientificAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitNotScientific(ExpressionParser.NotScientificContext ctx) { }
+	@Override public void exitScientificAtom(ExpressionParser.ScientificAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterNegateAtom(ExpressionParser.NegateAtomContext ctx) { }
+	@Override public void enterNotScientific(ExpressionParser.NotScientificContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitNegateAtom(ExpressionParser.NegateAtomContext ctx) { }
+	@Override public void exitNotScientific(ExpressionParser.NotScientificContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterSimpleAtom(ExpressionParser.SimpleAtomContext ctx) { }
+	@Override public void enterNegateAtom(ExpressionParser.NegateAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitSimpleAtom(ExpressionParser.SimpleAtomContext ctx) { }
+	@Override public void exitNegateAtom(ExpressionParser.NegateAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterPositiveAtom(ExpressionParser.PositiveAtomContext ctx) { }
+	@Override public void enterSimpleAtom(ExpressionParser.SimpleAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitPositiveAtom(ExpressionParser.PositiveAtomContext ctx) { }
+	@Override public void exitSimpleAtom(ExpressionParser.SimpleAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRationalNumber(ExpressionParser.RationalNumberContext ctx) { }
+	@Override public void enterPositiveAtom(ExpressionParser.PositiveAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRationalNumber(ExpressionParser.RationalNumberContext ctx) { }
+	@Override public void exitPositiveAtom(ExpressionParser.PositiveAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterIntergerAtom(ExpressionParser.IntergerAtomContext ctx) { }
+	@Override public void enterRationalNumber(ExpressionParser.RationalNumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitIntergerAtom(ExpressionParser.IntergerAtomContext ctx) { }
+	@Override public void exitRationalNumber(ExpressionParser.RationalNumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRealAtom(ExpressionParser.RealAtomContext ctx) { }
+	@Override public void enterIntergerAtom(ExpressionParser.IntergerAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRealAtom(ExpressionParser.RealAtomContext ctx) { }
+	@Override public void exitIntergerAtom(ExpressionParser.IntergerAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterPiNumber(ExpressionParser.PiNumberContext ctx) { }
+	@Override public void enterRealAtom(ExpressionParser.RealAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitPiNumber(ExpressionParser.PiNumberContext ctx) { }
+	@Override public void exitRealAtom(ExpressionParser.RealAtomContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRationalAtom(ExpressionParser.RationalAtomContext ctx) { }
+	@Override public void enterPiNumber(ExpressionParser.PiNumberContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRationalAtom(ExpressionParser.RationalAtomContext ctx) { }
+	@Override public void exitPiNumber(ExpressionParser.PiNumberContext ctx) {
+		// Empty on purpose
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRationalAtom(ExpressionParser.RationalAtomContext ctx) {
+		// Empty on purpose
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRationalAtom(ExpressionParser.RationalAtomContext ctx) {
+		// Empty on purpose
+	}
 
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	@Override public void enterEveryRule(ParserRuleContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	@Override public void exitEveryRule(ParserRuleContext ctx) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void visitTerminal(TerminalNode node) { }
+	@Override public void visitTerminal(TerminalNode node) {
+		// Empty on purpose
+	}
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void visitErrorNode(ErrorNode node) { }
+	@Override public void visitErrorNode(ErrorNode node) {
+		// Empty on purpose
+	}
 }
