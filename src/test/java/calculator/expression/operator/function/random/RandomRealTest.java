435aacef4730c379063e496f2439daad45f55581
amanzio <200291@umons.ac.be>
2025-05-13 21:06:28 +0200

    Add: random real function.

---
+++ src/test/java/calculator/expression/operator/function/random/RandomRealTest.java
@@ -0,0 +1,53 @@
+package calculator.expression.operator.function.random;
+
+import calculator.Calculator;
+import calculator.expression.Expression;
+import calculator.expression.number.MyComplex;
+import calculator.expression.number.MyRational;
+import calculator.expression.number.MyReal;
+import calculator.expression.operator.basic.Plus;
+import calculator.expression.operator.random.RandomReal;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class RandomRealTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testRandomReal() {
+        try {
+            final Expression noThrow = new RandomReal(new MyReal(BigDecimal.valueOf(9.9)));
+            final Expression wrap1 = new Plus(Arrays.asList(new MyReal(BigDecimal.ZERO), noThrow));
+            assertDoesNotThrow(()->calculator.eval(noThrow));
+            assertDoesNotThrow(()->Double.parseDouble(calculator.eval(wrap1)));
+
+            final Expression noThrow2 = new RandomReal(new MyComplex(BigDecimal.valueOf(10), BigDecimal.valueOf(0)));
+            final Expression wrap2 = new Plus(Arrays.asList(new MyReal(BigDecimal.ZERO), noThrow2));
+            assertDoesNotThrow(()->calculator.eval(noThrow2));
+            assertDoesNotThrow(()->Double.parseDouble(calculator.eval(wrap2)));
+
+            final Expression noThrow3 = new RandomReal(new MyRational(BigDecimal.valueOf(10), BigDecimal.valueOf(3)));
+            final Expression wrap3 = new Plus(Arrays.asList(new MyReal(BigDecimal.ZERO), noThrow3));
+            assertDoesNotThrow(()->calculator.eval(noThrow3));
+            assertDoesNotThrow(()->Double.parseDouble(calculator.eval(wrap3)));
+
+            assertTrue(Double.parseDouble(calculator.eval(wrap1)) <= 1);
+            assertTrue(Double.parseDouble(calculator.eval(wrap1)) >= 0);
+
+            assertTrue(Double.parseDouble(calculator.eval(wrap2)) <= 1);
+            assertTrue(Double.parseDouble(calculator.eval(wrap2)) >= 0);
+
+            assertTrue(Double.parseDouble(calculator.eval(wrap3)) <= 1);
+            assertTrue(Double.parseDouble(calculator.eval(wrap3)) >= 0);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
