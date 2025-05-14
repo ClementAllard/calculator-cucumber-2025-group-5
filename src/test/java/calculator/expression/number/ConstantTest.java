369529010febf45b8fc099c8ee42166a122652dc
amanzio <200291@umons.ac.be>
2025-05-14 17:23:09 +0200

    Add: constant tests.

---
+++ src/test/java/calculator/expression/number/ConstantTest.java
@@ -0,0 +1,36 @@
+package calculator.expression.number;
+
+import calculator.Calculator;
+import calculator.expression.Expression;
+import helper.IllegalSyntax;
+import jdk.jshell.spi.ExecutionControl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstantTest {
+
+    @Test
+    void testNoInfiniteLoop() {
+        String constantValue = "$bug$";
+        String constantKey = "bug";
+        Constant.writeConstant(constantKey, constantValue);
+
+        assertThrowsExactly(IllegalArgumentException.class, () ->
+                Constant.parseExpression(Constant.readConstant(constantKey)));
+    }
+
+    @Test
+    void testConstant() throws IllegalSyntax, ExecutionControl.NotImplementedException {
+        String constantValue = "log(2, 2^12) - 12";
+        String constantKey = "test";
+        Constant.writeConstant(constantKey, constantValue);
+
+        assertEquals(constantValue, Constant.readConstant(constantKey));
+
+        Calculator calculator = new Calculator();
+        Expression expression = Constant.parseExpression(Constant.readConstant(constantKey));
+
+        assertEquals("0", calculator.eval(expression));
+    }
+}
